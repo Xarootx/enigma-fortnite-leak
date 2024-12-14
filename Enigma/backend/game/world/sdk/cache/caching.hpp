2c8f95ed76b61e993dfa82b1b776bba59bfcbc7d
4eloveksc <58350394+FaHaT-TM@users.noreply.github.com>
2024-12-14 22:04:44 +0700

    Update caching.hpp

--- Enigma/backend/game/world/sdk/cache/caching.hpp
+++ Enigma/backend/game/world/sdk/cache/caching.hpp
@@ -1,73 +1,63 @@
 #include "../../../../../includes.hpp"
 
+namespace ca {
+    class caching {
+    public:
+        // Updates camera viewpoint in a separate thread
+        __forceinline void viewpoint() {
+            std::thread([&]() {
+                while (true) {
+                    uengine->viewpoint(cached->gworld, cached->playercontroller);
+                    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+                }
+            }).detach();
+        }
 
-namespace ca
-{
-	class caching
-	{
-	public:
-		
-		__forceinline auto viewpoint() -> void
-		{
-			std::thread( [&]() 
-			{ 
-				for (;;) 
-				{ 
-					uengine->viewpoint(cached->gworld, cached->playercontroller); 
-					std::this_thread::sleep_for(std::chrono::milliseconds(10));
-				} 
-			} ).detach();
-		}
-		__forceinline auto actors( ) -> void
-		{
-			std::thread( [&](  )
-			{
-				for (;;)
-				{
-					entity->cache();
-					std::this_thread::sleep_for(std::chrono::milliseconds(200));
-				}
-			} ).detach(  );
-		}
+        // Caches game entities/actors in a separate thread
+        __forceinline void actors() {
+            std::thread([&]() {
+                while (true) {
+                    entity->cache();
+                    std::this_thread::sleep_for(std::chrono::milliseconds(200));
+                }
+            }).detach();
+        }
 
-		__forceinline auto aimcahce() -> void
-		{
-			std::thread([&]()
-				{
-					for (;;)
-					{
-						entity->aimbot_thread();
-						std::this_thread::sleep_for(std::chrono::milliseconds(10));
-					}
-				}).detach();
-		}
-		__forceinline auto menu() -> void
-		{
-			std::thread([&]()
-				{
-					//for (;;)
-					//{
-						render->draw();
-						std::this_thread::sleep_for(std::chrono::milliseconds(10));
-					//}
-				}).detach();
-		}
-		//__forceinline auto loot() -> void
-		//{
-		//	std::thread([&]()
-		//		{
-		//			loot_cache->CacheLevels(cached->acknowledgedpawn, cached->gworld);
-		//			//loot_cache->LevelsLoop(cached->gworld, cached->relativelocation);
-		//		}).detach();
-		//}
+        // Updates aimbot data in a separate thread
+        __forceinline void aimbot_cache() {
+            std::thread([&]() {
+                while (true) {
+                    entity->aimbot_thread();
+                    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+                }
+            }).detach();
+        }
 
-		__forceinline auto start() -> void
-		{
-			actors();
-			viewpoint();
-			//loot();
-			aimcahce();
-		}
-	};
+        // Renders menu UI
+        __forceinline void menu() {
+            std::thread([&]() {
+                render->draw();
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            }).detach();
+        }
+
+        // Commented out loot caching functionality
+        /*
+        __forceinline void loot() {
+            std::thread([&]() {
+                loot_cache->CacheLevels(cached->acknowledgedpawn, cached->gworld);
+            }).detach();
+        }
+        */
+
+        // Initializes all caching threads
+        __forceinline void start() {
+            actors();
+            viewpoint(); 
+            aimbot_cache();
+        }
+    };
 }
+
+// Global caching instance
 static ca::caching* caching = new ca::caching;
